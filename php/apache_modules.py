eea5dc0ed10f53cef36ebc280fb1caf32d735b1e
Alessandro Corbelli <alessandro.corbelli@guest.it>
2014-01-21 10:35:09 +0100

    Add support for installing apache modules through app.yaml

---
+++ php/apache_modules.py
@@ -0,0 +1,44 @@
+import os
+
+import yaml
+
+
+def load_modules(data):
+    result = yaml.load(data)
+    if result:
+        apache = result.get('apache', {})
+        if apache:
+            return apache.get('modules', [])
+    return []
+
+
+def install_modules(modules):
+    installed = 0
+    for module in modules:
+        print "   Installing Apache module %s" % module
+        os.system("/usr/sbin/a2enmod "+module+" >/dev/null")
+        installed = installed + 1
+
+    if installed > 0: 
+       os.system('/etc/init.d/apache2 restart')
+       print "   Done installing Apache modules."
+    else:
+       print "   No Apache modules to install."
+
+def load_file(working_dir="/home/application/current"):
+    files_name = ["app.yaml", "app.yml"]
+    for file_name in files_name:
+        try:
+            with open(os.path.join(working_dir, file_name)) as f:
+                return f.read()
+        except IOError:
+            pass
+    return ""
+
+
+def main():
+    data = load_file()
+    modules = load_modules(data)
+    install_modules(modules)
+
+main()
