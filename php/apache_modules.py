cc49a059bc5f5e12a4deff7f9a17b73a48b65296
GUEST.it s.r.l <assistenza@guest.it>
2014-03-05 18:56:54 +0100

    More fixes on deploy output

--- php/apache_modules.py
+++ php/apache_modules.py
@@ -15,15 +15,15 @@ def load_modules(data):
 def install_modules(modules):
     installed = 0
     for module in modules:
-        print '\e[1G\e[K'+"      Installing Apache module %s" % module
+        print " Installing Apache module %s" % module
         os.system("a2enmod "+module+" >/dev/null 2>&1")
         installed = installed + 1
 
     if installed > 0: 
        os.system('/etc/init.d/apache2 restart >/dev/null 2>&1')
-       print '\e[1G\e[K'+"      Done installing Apache modules."
+       print " Done installing Apache modules."
     else:
-       print '\e[1G\e[K'+"      No Apache modules installed."
+       print " No Apache modules installed."
 
 
 def load_file(working_dir="/home/application/current"):
@@ -38,7 +38,7 @@ def load_file(working_dir="/home/application/current"):
 
 
 def main():
-    print '\e[1G\e[K'+"----> Parsing required Apache modules"
+    print "Parsing required Apache modules"
     data = load_file()
     modules = load_modules(data)
     install_modules(modules)
