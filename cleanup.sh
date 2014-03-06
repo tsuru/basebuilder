bd77b1063257cf08a05e4573b42656c3fcbbe7f5
GUEST.it s.r.l <assistenza@guest.it>
2014-03-06 11:13:07 +0100

    Moved circus.ini Add cleanup.sh script

---
+++ cleanup.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+SOURCE_DIR=$1
+PLATFORM=2
+
+# Cleanup environment removing all except needed platform from /var/lib/tsuru
+cd ${SOURCE_DIR}
+
+rm -v Makefile README.* .gitignore
+find -type d ! -path ${PLATFORM} ! -path base
+
+# Self destruction
+rm -v $0
