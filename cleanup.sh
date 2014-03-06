1b1caee034a4a2aab2070836bdf8eba78329c615
GUEST.it s.r.l <assistenza@guest.it>
2014-03-06 12:15:05 +0100

    Fix cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -5,9 +5,8 @@ PLATFORM=$2
 # Cleanup environment removing all except needed platform from /var/lib/tsuru
 cd ${SOURCE_DIR}
 
-rm -v Makefile README.* .gitignore
 rm -rfv $(find . -maxdepth 1 -type d | egrep -v './base|./php')
 ls -la ${SOURCE_DIR}
 
-# Self destruction
-rm -v $0
+# Remove files + Self destruction
+rm -v Makefile README.* .gitignore cleanup.sh
