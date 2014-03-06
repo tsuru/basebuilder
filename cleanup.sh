41ace3348c0ce74948a9434826b8c7ca5386e2d6
GUEST.it s.r.l <assistenza@guest.it>
2014-03-06 19:21:51 +0100

    Improvements when cleaning environment

--- cleanup.sh
+++ cleanup.sh
@@ -8,3 +8,13 @@ cd ${SOURCE_DIR}
 # Remove files + Self destruction
 find . -maxdepth 1 -type d | grep -vP "^\.$|\.\/base|\.\/${PLATFORM}" | xargs rm -rf
 rm Makefile README.* .gitignore $0
+
+# Cleaning up environment
+apt-get autoremove -y --force-yes
+apt-get clean -y --force-yes
+rm -rf /var/cache/apt/archives/*.deb
+rm -rf /var/lib/apt/lists/*
+rm -rf /root/*
+rm -rf /tmp/*
+rm -rf /usr/share/doc
+rm -rf /usr/shar/man
