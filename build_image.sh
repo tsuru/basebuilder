c839e2e461a44800925e90e35dd9cec081e45be2
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2014-04-07 09:57:56 -0300

    Script to simplify the creation of images using a common Dockerfile

---
+++ build_image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -el
+
+# Script to build docker image using Dockerfile.template
+#
+# Usage: ./build_image.sh <image_name>
+#
+# There should be a directory named <image_name> containing
+# only the hooks you wish to override from base
+#
+
+IMAGE_NAME=$1
+BUILD_DIR=/tmp/image_build/${IMAGE_NAME}_$$
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "Building $DIR/$IMAGE_NAME in $BUILD_DIR..."
+
+mkdir -p $BUILD_DIR
+cp -a $DIR/* $BUILD_DIR
+sed -e "s/{{IMAGE_NAME}}/${IMAGE_NAME}/" $DIR/Dockerfile.template > $BUILD_DIR/Dockerfile
+docker build -t "tsuru/$IMAGE_NAME" $BUILD_DIR
+rm -rf $BUILD_DIR
