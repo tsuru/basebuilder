7b5e742f162e4cf3e6180334995154add6fa9ccd
Francisco Souza <fss@corp.globo.com>
2014-06-16 18:27:35 -0300

    tests/platforms: change the running process

--- tests/run.bash
+++ tests/run.bash
@@ -1,3 +1,11 @@
 #!/bin/bash -el
 
-sudo -iu vagrant /vagrant/platforms.bash
+args=""
+
+case $1 in
+	pre_receive_archive)
+		args="--archive-server --hook-url https://raw.githubusercontent.com/tsuru/tsuru/master/misc/git-hooks/pre-receive.archive-server --hook-name pre-receive"
+		;;
+esac
+
+sudo -iu vagrant /vagrant/platforms.bash $args
