e8f10a0400409c4582b4a01f176f7608e3089d12
Francisco Souza <fss@corp.globo.com>
2014-06-18 18:16:47 -0300

    Vagrantfile, run: support s3cmd pre-receive hook

--- tests/run.bash
+++ tests/run.bash
@@ -14,6 +14,9 @@ case $1 in
 	pre_receive_swift)
 		args=(--hook-url https://raw.githubusercontent.com/tsuru/tsuru/master/misc/git-hooks/pre-receive.swift --hook-name pre-receive --env AUTH_URL "${SWIFT_AUTH_URL}" --env AUTH_PARAMS "${SWIFT_AUTH_PARAMS}" --env CONTAINER_NAME ${SWIFT_CONTAINER_NAME})
 		;;
+	pre_receive_s3)
+		args=(--hook-url https://raw.githubusercontent.com/tsuru/tsuru/master/misc/git-hooks/pre-receive.s3cmd --hook-name pre-receive --env BUCKET_NAME $S3_BUCKET_NAME --aws-access-key $AWS_ACCESS_KEY --aws-secret-key $AWS_SECRET_KEY)
+		;;
 esac
 
 sudo -iu vagrant /vagrant/platforms.bash "${args[@]}"
