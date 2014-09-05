7a9f054eb2845388a0ace8d5679b9aa1552bf4e7
Francisco Souza <fss@corp.globo.com>
2014-09-04 22:07:11 -0300

    tests/platforms: add ruby21

--- tests/platforms.bash
+++ tests/platforms.bash
@@ -91,7 +91,7 @@ set -e
 clone_basebuilder /tmp/basebuilder
 echo -e "Host localhost\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
-platforms="java nodejs php python python3 ruby ruby20 static buildpack cordova lisp"
+platforms="java nodejs php python python3 ruby ruby20 ruby21 static buildpack cordova lisp"
 
 for platform in $platforms
 do
