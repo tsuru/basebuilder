7327541346ed156c0249ce6858a4d15cb2c90d79
Francisco Souza <fss@corp.globo.com>
2015-04-15 21:17:53 -0300

    tests/platforms: add iojs to the set of platforms to be tested

--- tests/platforms.bash
+++ tests/platforms.bash
@@ -96,7 +96,7 @@ set -e
 clone_basebuilder /tmp/basebuilder
 echo -e "Host localhost\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
-platforms="java nodejs php python python3 ruby static cordova play"
+platforms="java nodejs php python python3 ruby static cordova play iojs"
 
 for platform in $platforms
 do
