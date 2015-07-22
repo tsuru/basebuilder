13bfff79b1f424ed77e071669c67c3bc5d2ff04f
Francisco Souza <fss@corp.globo.com>
2015-07-22 10:57:14 -0300

    tests/platforms: force running with nightly packages
    The default behavior of tsuru-now changed to install stable packages.

--- tests/platforms.bash
+++ tests/platforms.bash
@@ -87,7 +87,7 @@ sudo -E apt-get install curl -qqy
 sudo -E apt-get update
 sudo -E apt-get install linux-image-extra-$(uname -r) -qqy
 curl -sL https://raw.githubusercontent.com/tsuru/now/master/run.bash -o /tmp/tsuru-now.bash
-bash /tmp/tsuru-now.bash "$@" --without-dashboard
+bash /tmp/tsuru-now.bash "$@" --without-dashboard --tsuru-pkg-nightly
 
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$PATH
