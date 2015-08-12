b329f5195fd85cd7bab49621646aaffd04450ee7
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2015-08-12 15:59:47 -0300

    install s3cmd to pre_receive_s3 hook

--- tests/platforms.bash
+++ tests/platforms.bash
@@ -80,6 +80,57 @@ function clean_tsuru_now() {
 	docker rmi -f tsuru/python 2>/dev/null
 }
 
+function install_s3cmd {
+    sudo apt-get install s3cmd python-magic -y
+    cat > /tmp/s3cfg <<END
+[default]
+access_key = ${AWS_ACCESS_KEY}
+bucket_location = US
+cloudfront_host = cloudfront.amazonaws.com
+default_mime_type = binary/octet-stream
+delete_removed = False
+dry_run = False
+enable_multipart = True
+encoding = ANSI_X3.4-1968
+encrypt = False
+follow_symlinks = False
+force = False
+get_continue = False
+gpg_command = /usr/bin/gpg
+gpg_decrypt = %(gpg_command)s -d --verbose --no-use-agent --batch --yes --passphrase-fd %(passphrase_fd)s -o %(output_file)s %(input_file)s
+gpg_encrypt = %(gpg_command)s -c --verbose --no-use-agent --batch --yes --passphrase-fd %(passphrase_fd)s -o %(output_file)s %(input_file)s
+gpg_passphrase =
+guess_mime_type = True
+host_base = s3.amazonaws.com
+host_bucket = %(bucket)s.s3.amazonaws.com
+human_readable_sizes = False
+invalidate_on_cf = False
+list_md5 = False
+log_target_prefix =
+mime_type =
+multipart_chunk_size_mb = 15
+preserve_attrs = True
+progress_meter = True
+proxy_host =
+proxy_port = 0
+recursive = False
+recv_chunk = 4096
+reduced_redundancy = False
+secret_key = ${AWS_SECRET_KEY}
+send_chunk = 4096
+simpledb_host = sdb.amazonaws.com
+skip_existing = False
+socket_timeout = 300
+urlencoding_mode = normal
+use_https = True
+verbosity = WARNING
+website_endpoint = http://%(bucket)s.s3-website-%(location)s.amazonaws.com/
+website_error =
+website_index = index.html
+END
+    sudo mv /tmp/s3cfg ~git/.s3cfg
+}
+
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get install curl -qqy
 
@@ -103,6 +154,7 @@ case $1 in
         envs=('BUCKET_NAME=$S3_BUCKET_NAME')
         export AWS_ACCESS_KEY=$AWS_ACCESS_KEY
         export AWS_SECRET_KEY=$AWS_SECRET_KEY
+        install_s3cmd
 		;;
 esac
 
